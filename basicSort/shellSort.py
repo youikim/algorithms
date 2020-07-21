dc5f4e0dd951651589b0981a390582bd92e5cd3a
영섭 <0331csr@ezcaretech.com>
2020-07-21 14:13:17 +0900

    ## 셀정렬 수정사항

---
+++ basicSort/shellSort.py
@@ -0,0 +1,14 @@
+def shellSort(a, n):
+    h = 1
+    while h < n:  ## 첫번째 간격 계산
+        h = 3 * h + 1
+
+    while h > 0:
+        for i in range(h + 1, n + 1):
+            v, j = a[i], i  ## v: value, j: index
+            #             print(v,j,h,a[j-h])
+            while j > h and a[j - h] > v:  ## 앞의 값 a[j-h]와 뒤의 값(v) 비교
+                a[j] = a[j - h]  ## 값 교환
+                j = j - h
+            a[j] = v
+        h = int(h / 3)  ## 간격 재 조정
